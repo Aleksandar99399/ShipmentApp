cc8e06744f4ccf5cd99e742edf3e25b0f4196824
Aleksandar99399 <aleksandar_unchev@abv.bg>
2020-08-13 09:28:34 +0300

    commit

--- finalExamWebAdvanced/src/test/java/finalproject/service/UserServiceTest.java
+++ finalExamWebAdvanced/src/test/java/finalproject/service/UserServiceTest.java
@@ -7,38 +7,45 @@ import finalproject.models.serviceModels.UserServiceModel;
 
 import finalproject.repositories.UserRepository;
 
+import finalproject.services.TownService;
 import finalproject.services.UserService;
 
 
-import org.junit.jupiter.api.AfterEach;
-
-
+import finalproject.services.impl.UserServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-
-import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 
 import java.util.List;
 import java.util.Optional;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTest  {
 
-
-    @Autowired
-    private UserService userService;
+    private UserService serviceToTest;
 
     @Mock
-    private UserRepository userRepository;
+    private UserRepository mockUserRepository;
 
-    @AfterEach
-    public void delete() {
-        this.userRepository.deleteAll();
+    @Mock
+    private TownService townService;
+
+    @BeforeEach
+    public void setUp() {
+        serviceToTest = new UserServiceImpl(mockUserRepository,
+                new ModelMapper(),
+                new BCryptPasswordEncoder(),
+                townService);
     }
 
     private User getUser() {
@@ -57,16 +64,16 @@ public class UserServiceTest  {
 
     @Test
     public void findUserByEmailCorrect() {
-        User user = getUser();
-
-        Mockito.when(this.userRepository.findByEmail("divoto@abv.bg")).thenReturn(Optional.of(user));
+        //arrange
+        User testUser = getUser();
+        Mockito.when(this.mockUserRepository.findByEmail(testUser.getEmail())).thenReturn(Optional.of(testUser));
 
         //act
-        UserServiceModel userServiceModel = this.userService.emailNotExist("divoto@abv.bg");
+        UserServiceModel userServiceModel = this.serviceToTest.emailNotExist(testUser.getEmail());
 
 
         //assert
-        assertEquals("yes", userServiceModel.getEmail(), user.getEmail());
+        Assertions.assertEquals(userServiceModel.getEmail(), testUser.getEmail());
 
 //        //ARRANGE
 //        Optional<User> byEmail = this.userRepository.findByEmail("divoto@abv.bg");
